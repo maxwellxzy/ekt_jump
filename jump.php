8c944146dab4bf76977b996345f31aa9b14488bb
maxwell <maxwellxzy@gmail.com>
2019-09-13 14:04:56 +0000

    第一次提交

---
+++ jump.php
@@ -0,0 +1,12 @@
+<?php
+/*每天18点到次日7点访问维护页面，其余时间访问正常网站*/
+	$hour = date(H);
+	if($hour > 17 || $hour < 7){
+		header("location:update.html");
+	}
+	else{
+		header("location:http://ekt.szedu.com/dist/index.html#/");
+	}
+
+?>
+
